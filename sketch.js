aabe18e421a57eda20444238f8f1d1f2548c3d64
davidlnowak <nowak.david@gmail.com>
2016-03-22 03:30:09 -0400

    Complete Working Application
    All coding complete for this daily.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
 var bubbles = [];
 
+var loadBubbles = function(totalBubbles) {
+  for (var index = 0; index < totalBubbles; ++index) {
+    bubbles.push(new Bubble(random(25, width-25), random(height/2-100, height/2+100)))
+  }
+};
+
 setup = function () {
   createCanvas(600, 600);
-  // your code goes here
+
+  loadBubbles(500);
 };
 
 draw = function () {
   background(0);
-  // your code goes here
+
+  bubbles.forEach(function updateAndDisplay (bubble) {
+  bubble.display();
+  bubble.update();
+});
 };
